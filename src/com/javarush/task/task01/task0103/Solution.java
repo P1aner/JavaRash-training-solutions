40e5ab5e172f3b41b15b9dc42a56df1472aa40f3
-PLANER-DESKTOP\pavel <pavel.planer@gmail.com>
2021-11-22 23:44:06 +0300

    0103

---
+++ src/com/javarush/task/task01/task0103/Solution.java
@@ -0,0 +1,14 @@
+package com.javarush.task.task01.task0103;
+
+/*
+Мой юный друг
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        int i = 3126;
+        int y = i-8;
+        System.out.print(y);
+        //напишите тут ваш код
+    }
+}
