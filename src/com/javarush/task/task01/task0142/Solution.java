6c83dbff17beb7b5ee5e3857fab4c5dd493495e6
-PLANER-DESKTOP\pavel <pavel.planer@gmail.com>
2021-11-22 23:12:34 +0300

    Popravil

--- src/com/javarush/task/task01/task0142/Solution.java
+++ src/com/javarush/task/task01/task0142/Solution.java
@@ -1,9 +1,8 @@
 package com.javarush.task.task01;
 
 
-public class task0142 {
+public class Solution {
     public static void main(String[] args) {
         System.out.print("Амиго очень умный");
     }
 }
-
