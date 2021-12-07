9837b846006f3ec770a0ba2b41efc3bbd564cb0f
-PLANER-DESKTOP\pavel <pavel.planer@gmail.com>
2021-12-07 15:55:32 +0300

    task209

---
+++ src/com/javarush/task/task02/task0208/Solution.java
@@ -0,0 +1,47 @@
+package com.javarush.task.task02.task0208;
+
+/*
+Одного кота нам мало
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        //напишите тут ваш код
+        Cat barsik = new Cat();
+        Cat mursik = new Cat();
+    }
+
+    public static class Cat {
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
