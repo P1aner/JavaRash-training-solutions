e1d7c8c62c2a8f6dc44353086afb4e46db6b434d
-PLANER-DESKTOP\pavel <pavel.planer@gmail.com>
2021-12-07 16:27:05 +0300

    task210

---
+++ src/com/javarush/task/task02/task0210/Solution.java
@@ -0,0 +1,41 @@
+package com.javarush.task.task02.task0210;
+
+/*
+Необъективная реальность
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println("Будешь плохо кодить, придет Java и съест твою память");
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
