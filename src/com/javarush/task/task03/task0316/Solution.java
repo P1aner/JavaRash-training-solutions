c3b2c3f19a8621d546353720112d1e9a54d88e3f
-PLANER-DESKTOP\pavel <pavel.planer@gmail.com>
2021-12-08 22:54:16 +0300

    lvl 3.6

---
+++ src/com/javarush/task/task03/task0316/Solution.java
@@ -0,0 +1,24 @@
+package com.javarush.task.task03.task0316;
+
+/*
+Экранирование символов
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        //напишите тут ваш код
+        System.out.println("It's a Windows path: \"C:\\Program Files\\Java\\jdk1.7.0\\bin\"");
+        System.out.println("It's a Java string: \\\"C:\\\\Program Files\\\\Java\\\\jdk1.7.0\\\\bin\\\"");
+    }
+}
+
+/*Вывести на экран следующий текст - две строки:
+It's a Windows path: "C:\Program Files\Java\jdk1.7.0\bin"
+It's a Java string: \"C:\\Program Files\\Java\\jdk1.7.0\\bin\"
+
+Подсказка:
+\” – экранирование двойной кавычки;
+\\ – экранирование обратной косой черты (\).
+
+Больше про экранирование символов и Escape-последовательности в Java читай в статье.
+*/
