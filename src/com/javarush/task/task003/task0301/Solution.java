a86c556f317a7734c871776da29f01babd119ea1
-PLANER-DESKTOP\pavel <pavel.planer@gmail.com>
2021-12-08 13:16:22 +0300

    lvl 3 lesson 1

---
+++ src/com/javarush/task/task003/task0301/Solution.java
@@ -0,0 +1,23 @@
+package com.javarush.task.task03.task0301;
+
+/*
+Делиться полезно
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        div(6, 3);
+        div(10, 6);
+        div(2, 4);
+    }
+
+    public static void div(int a, int b) {
+
+        System.out.println(a * 1.0 / b);//напишите тут ваш код
+
+    }
+}
+
+/*Реализуй метод public static void div(int a, int b).
+Метод должен делить первое число на второе, и выводить на экран результат деления а на b.
+На экран должно быть выведено целое число.*/
