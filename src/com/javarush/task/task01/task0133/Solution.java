0e9e7832c9e2ac3c4acbfdc7eedbf12213f4301b
-PLANER-DESKTOP\pavel <pavel.planer@gmail.com>
2021-12-10 21:01:02 +0300

    lvl 3.12 v2 ускользающее время

--- src/com/javarush/task/task01/task0133/Solution.java
+++ src/com/javarush/task/task01/task0133/Solution.java
@@ -4,10 +4,37 @@ package com.javarush.task.task01.task0133;
 Не думать о секундах…
 */
 
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+
 public class Solution {
-    public static void main(String[] args) {
-        int secondsAfter15 = 30 * 60;
+    public static void main(String[] args) throws Exception {
+       /* int secondsAfter15 = 30 * 60;
         System.out.println(secondsAfter15);
+    }*/
+        System.out.println("Введите время в формате hh.mm:");
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String text = reader.readLine();
+        double time1 = Double.parseDouble(text);
+
+        System.out.println("Введите время 2 в формате hh.mm:");
+        text = reader.readLine();
+        double time2 = Double.parseDouble(text);
+        int h1 = (int) time1;
+        int h2 = (int) time2;
+        double m1 = ((time1 - h1) * 100);
+        double m2 = ((time2 - h2) * 100);
+        int mm1 = (int) m1;
+        int mm2 = (int) m2;
+        mm1 = h1 * 60 + mm1;
+        mm2 = h2 * 60 + mm2;
+        int mm = (mm2 - mm1)*60;
+
+
+        System.out.println(mm);
+
     }
 }
 
