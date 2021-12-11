ca653cf970d1a29ea7436b6f618d64fd714f2388
-PLANER-DESKTOP\pavel <pavel.planer@gmail.com>
2021-12-11 15:44:49 +0300

    lvl 4.2 v2

--- src/com/javarush/task/task04/task0407/Solution.java
+++ src/com/javarush/task/task04/task0407/Solution.java
@@ -7,24 +7,32 @@ package com.javarush.task.task04.task0407;
 public class Solution {
     public static void main(String[] args) {
         Cat cat1 = new Cat();
-        cat1.count = Cat.count + 1;
+
         //напишите тут ваш код
 
         Cat cat2 = new Cat();
         //напишите тут ваш код
-        cat2.count = Cat.count + 1;
+
 
         System.out.println("The cat count is " + Cat.count);
     }
 
     public static class Cat {
         public static int count = 0;
+
+        public Cat() {
+            Cat.count++;
+        }
+
+        ;
+
+
     }
 }
 
 
 /*Написать код, чтобы правильно считалось количество созданных котов (count) и на экран выдавалось правильное их количество.
-* */
+ * */
 
 
 /*The cat count is 2
