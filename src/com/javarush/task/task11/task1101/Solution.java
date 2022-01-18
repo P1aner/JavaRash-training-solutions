ea862f94f81a3b352206b912b04937595772d058
-PLANER-DESKTOP\pavel <pavel.planer@gmail.com>
2022-01-19 00:47:00 +0300

    lvl 11.6

---
+++ src/com/javarush/task/task11/task1101/Solution.java
@@ -0,0 +1,26 @@
+package com.javarush.task.task11.task1101;
+
+/*
+Лошадь и Пегас
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+    }
+
+    public class Horse {
+
+    }
+
+    public class Pegasus extends Horse {
+
+    }
+}
+/*
+Отредактируй два класса — Horse (лошадь) и Pegasus (пегас).
+Унаследуй пегаса от лошади.
+ */
+/*
+
+Process finished with exit code 0
+ */
