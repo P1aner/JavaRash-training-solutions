7f6347a493a7dc0cf0d7283ce29555fa113f4274
-PLANER-DESKTOP\pavel <pavel.planer@gmail.com>
2022-01-19 01:59:26 +0300

    lvl 11.11 v0

---
+++ src/com/javarush/task/task11/task1115/Solution.java
@@ -0,0 +1,35 @@
+package com.javarush.task.task11.task1115;
+
+/*
+От школьника до пенсии
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+    }
+
+    public class Schoolboy {
+
+    }
+
+    public class Student extends Schoolboy {
+
+    }
+
+    public class Worker extends Student {
+
+    }
+
+    public class Retiree extends Worker { //пенсионер с методами школьника ммм, дергать за косички и бегать по партам xD
+
+    }
+
+}
+/*
+Измени четыре класса: Schoolboy (школьник), Student (студент), Worker (Сотрудник), Retiree (Пенсионер).
+Унаследовать студента от школьника, сотрудника от студента, пенсионера от сотрудника.
+ */
+/*
+
+Process finished with exit code 0
+ */
