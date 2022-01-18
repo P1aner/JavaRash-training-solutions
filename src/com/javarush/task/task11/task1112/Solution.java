24e0a7ca009fa044c3279282b0c2050a072dc5e1
-PLANER-DESKTOP\pavel <pavel.planer@gmail.com>
2022-01-19 01:08:31 +0300

    lvl 11.8

---
+++ src/com/javarush/task/task11/task1112/Solution.java
@@ -0,0 +1,27 @@
+package com.javarush.task.task11.task1112;
+
+/*
+iPhone и Apple
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+    }
+
+    public class Apple extends IPhone {
+
+    }
+
+    public class IPhone {
+
+    }
+}
+/*
+Измени два класса — Apple и IPhone.
+Унаследуй IPhone от Apple.
+ */
+/*
+
+Process finished with exit code 0
+ */
+
---
+++ src/com/javarush/task/task11/task1113/Solution.java
@@ -0,0 +1,35 @@
+package com.javarush.task.task11.task1113;
+
+/*
+Играем в Дарвина
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+    }
+
+    public class Fish {
+
+    }
+
+    public class Animal extends Fish {
+
+    }
+
+    public class Ape extends Animal {
+
+    }
+
+    public class Human extends Ape {
+
+    }
+
+}
+/*
+Измени четыре класса: Fish (Рыба), Animal (Животное), Ape (Обезьяна), Human (Человек).
+Унаследовать животное от рыбы, обезьяну — от животного и человека от обезьяны.
+ */
+/*
+
+Process finished with exit code 0
+ */
