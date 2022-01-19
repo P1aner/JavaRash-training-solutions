17eaf37117a85b85710383c7b9442b4bed9d411e
-PLANER-DESKTOP\pavel <pavel.planer@gmail.com>
2022-01-19 22:20:26 +0300

    lvl 12.4 v0

---
+++ src/com/javarush/task/task12/task1206/Solution.java
@@ -0,0 +1,29 @@
+package com.javarush.task.task12.task1206;
+
+/*
+Выполняем перегрузку!
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    //Напишите тут ваши методы
+
+    public static void print(int number){
+
+    }
+
+    public static void print(String text){
+
+    }
+}
+/*
+Напиши два метода: print(int) и print(String).
+ */
+/*
+
+Process finished with exit code 0
+ */
+
