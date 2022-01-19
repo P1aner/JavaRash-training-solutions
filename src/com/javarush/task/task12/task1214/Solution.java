d48459078d2d37ac59d75bdb60c46b3f674cfccf
-PLANER-DESKTOP\pavel <pavel.planer@gmail.com>
2022-01-19 23:58:26 +0300

    lvl 12.6 v0

---
+++ src/com/javarush/task/task12/task1214/Solution.java
@@ -0,0 +1,31 @@
+package com.javarush.task.task12.task1214;
+
+/*
+Корова — тоже животное
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    public static abstract class Animal {
+        public abstract String getName();
+    }
+
+    public static class Cow extends Animal {
+        @Override
+        public String getName() {
+            return null;
+        }
+    }
+
+}
+/*
+Унаследуй класс Cow от Animal.
+Реализуй все недостающие методы в классе Cow.
+ */
+/*
+
+Process finished with exit code 0
+ */
