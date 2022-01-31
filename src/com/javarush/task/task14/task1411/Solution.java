e9fd94626e1b9c9c73d9c70e2d4f09c5b63f4605
Павел <pavel.planer@gmail.com>
2022-01-31 16:30:39 +0300

    14.8 v1

---
+++ src/com/javarush/task/task14/task1411/Solution.java
@@ -0,0 +1,48 @@
+package com.javarush.task.task14.task1411;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+/*
+User, Loser, Coder and Proger
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        Person person = null;
+        String key = null;
+
+        //тут цикл по чтению ключей, пункт 1
+        while (true){
+            key= reader.readLine();
+
+
+        }
+        {
+            //создаем объект, пункт 2
+
+            doWork(person); //вызываем doWork
+
+        }
+    }
+
+    public static void doWork(Person person) {
+        // пункт 3
+    }
+}
+/*
+Давай напишем программу, которая определит, чем заняться тому или иному человеку.
+Для этого нужно:
+Ввести [в цикле] с клавиатуры несколько строк (ключей).
+Строки (ключи) могут быть такими: "user", "loser", "coder", "proger".
+Ввод окончен, когда строка не совпадает ни с одной из выше указанных.
+Для каждой введенной строки нужно:
+Создать соответствующий объект [см. Person.java], например, для строки "user" нужно создать объект класса User.
+Передать этот объект в метод doWork.
+Написать реализацию метода doWork, который:
+Вызывает метод live() у переданного объекта, если этот объект (person) имеет тип User.
+Вызывает метод doNothing(), если person имеет тип Loser.
+Вызывает метод writeCode(), если person имеет тип Coder.
+Вызывает метод enjoy(), если person имеет тип Proger.
+ */
