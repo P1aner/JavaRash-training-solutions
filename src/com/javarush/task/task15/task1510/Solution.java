ef7dccdf3968ac231bb88215e97e88eec802945a
Павел <pavel.planer@gmail.com>
2022-02-05 21:54:41 +0300

    15.4 v0

---
+++ src/com/javarush/task/task15/task1510/Solution.java
@@ -0,0 +1,38 @@
+package com.javarush.task.task15.task1510;
+
+/*
+Все лишнее - прочь!
+*/
+
+/**
+ * Убери в методе main() лишние строки, для которых метод add() не реализован.
+ */
+
+public class Solution {
+    public static void main(String[] args) {
+        add((short) 1, 2f);
+        add(1, 2);
+        add(2d, 2);
+        add((byte) 1, 2d);
+    }
+
+    public static void add(int i, int j) {
+        System.out.println("Integer addition");
+    }
+
+    public static void add(int i, double j) {
+        System.out.println("Integer and double addition");
+    }
+
+    public static void add(double i, double j) {
+        System.out.println("Double addition");
+    }
+}
+/*
+Integer and double addition
+Integer addition
+Double addition
+Integer and double addition
+
+Process finished with exit code 0
+ */
