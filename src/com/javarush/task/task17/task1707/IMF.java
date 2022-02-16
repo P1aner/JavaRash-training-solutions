5c9fc51f5a5357834daa9ecf174c11736d635433
Admin <pavel.planer@gmail.com>
2022-02-16 10:51:01 +0300

    17.4 v0

---
+++ src/com/javarush/task/task17/task1707/IMF.java
@@ -0,0 +1,22 @@
+package com.javarush.task.task17.task1707;
+
+
+public class IMF {
+
+    private static IMF imf;
+
+    public static IMF getFund() {
+        //add your code here - добавь код тут
+        synchronized (IMF.class) {
+            if (IMF.imf == null) {
+                imf = new IMF();
+            }
+        }
+        return imf;
+    }
+
+    private IMF() {
+    }
+}
+
+
