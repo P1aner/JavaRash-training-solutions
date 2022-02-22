430f630fc9231ba02e486aac5d4b1e71fea1e5ec
-PLANER-DESKTOP\pavel <pavel.planer@gmail.com>
2022-02-22 23:31:26 +0300

    lvl 17.10 v4

--- src/com/javarush/task/task14/task1419/Solution.java
+++ src/com/javarush/task/task14/task1419/Solution.java
@@ -1,8 +1,7 @@
 package com.javarush.task.task14.task1419;
 
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InterruptedIOException;
+import java.io.*;
+import java.net.ServerSocket;
 import java.net.SocketException;
 import java.util.ArrayList;
 import java.util.List;
@@ -28,21 +27,64 @@ public class Solution {
     }
 
     private static void initExceptions() {   //the first exception
+
         try {
             float i = 1 / 0;
+        } catch (ArithmeticException e) {
+            exceptions.add(e);
+        }
+        try {
+            FileReader reader = new FileReader("noFile.ok");
+        } catch (FileNotFoundException e) {
+            exceptions.add(e);
+        }
+        try {
+            String[] str = new String[0];
+            str[2] = "new";
+        } catch (ArrayIndexOutOfBoundsException e) {
+            exceptions.add(e);
+        }
+        try {
+            int i = Integer.parseInt("aga");
+        } catch (NumberFormatException e) {
+            exceptions.add(e);
+        }
+        try {
+            List<String> list = new ArrayList<>();
+            list.get(2);
+        } catch (IndexOutOfBoundsException e) {
+            exceptions.add(e);
+        }
+        try {
+            String text = null;
+            if (text == null) {
+                throw new NullPointerException();
+            }
+        } catch (NullPointerException e) {
+            exceptions.add(e);
+        }
+        try {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+            throw new IOException();
 
+        } catch (IOException e) {
+            exceptions.add(e);
+        }
+        try {
+            throw new SocketException();
+        } catch (SocketException e) {
+            exceptions.add(e);
+        }
+        try {
+            throw new RuntimeException();
+        } catch (RuntimeException e) {
+            exceptions.add(e);
+        }
+        try {
+            throw new Exception();
         } catch (Exception e) {
             exceptions.add(e);
         }
-        exceptions.add(new IOException());
-        exceptions.add(new FileNotFoundException());
-        exceptions.add(new SocketException());
-        exceptions.add(new RuntimeException());
-        exceptions.add(new IndexOutOfBoundsException());
-        exceptions.add(new IllegalAccessException());
-        exceptions.add(new NumberFormatException());
-        exceptions.add(new Exception());
-        exceptions.add(new ArrayIndexOutOfBoundsException());
         //напишите тут ваш код
 
     }
